7c3232c28843331ced4fd642649c86fc2eba463f
titus.renouf <titus.renouf@bluescale.com>
2022-12-05 15:44:35 +0100

    Home Page

--- main.py
+++ main.py
@@ -3,9 +3,19 @@ app = Flask(__name__)
 
 
 @app.route("/")
-@app.route("/home")
 def home():
-    return render_template("hello.html")
+    prefix_google = """
+    <!-- Google tag (gtag.js) -->
+    <script async
+    src="https://www.googletagmanager.com/gtag/js?id=G-PHZ8MBWLTY"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', ' G-PHZ8MBWLTY');
+    </script>
+    """
+    return prefix_google + render_template("hello.html")
 
 if __name__=="__main__":
     app.run(debug=True)
