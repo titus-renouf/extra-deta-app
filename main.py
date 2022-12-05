4eca260db7fe27894d5f0e3fa2222cc2cd6cc3dd
titus.renouf <titus.renouf@bluescale.com>
2022-12-05 15:05:24 +0100

    Button GA

--- main.py
+++ main.py
@@ -1,19 +1,17 @@
-from flask import Flask
-
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
-@app.route('/', methods=["GET"])
-def hello_world():
-    prefix_google = """
-    <!-- Google tag (gtag.js) -->
-    <script async
-    src="https://www.googletagmanager.com/gtag/js?id=G-PHZ8MBWLTY"></script>
-    <script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-    gtag('config', ' G-PHZ8MBWLTY');
-    </script>
-    """
-    return prefix_google + "Hello World"
+@app.route("/", methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        if request.form.get('action1') == 'VALUE1':
+            pass  # do something
+        elif request.form.get('action2') == 'VALUE2':
+            pass  # do something else
+        else:
+            pass  # unknown
+    elif request.method == 'GET':
+        return render_template('index.html', form=form)
+
+    return render_template("index.html")
