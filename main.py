387d31a04b053a468f7cbb56e5e1b30d1b4dc636
titus.renouf <titus.renouf@bluescale.com>
2022-12-05 15:03:41 +0100

    Connect to GA

--- main.py
+++ main.py
@@ -5,4 +5,15 @@ app = Flask(__name__)
 
 @app.route('/', methods=["GET"])
 def hello_world():
-    return "Hello World"
+    prefix_google = """
+    <!-- Google tag (gtag.js) -->
+    <script async
+    src="https://www.googletagmanager.com/gtag/js?id=G-PHZ8MBWLTY"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', ' G-PHZ8MBWLTY');
+    </script>
+    """
+    return prefix_google + "Hello World"
