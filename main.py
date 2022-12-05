a6a7fa12eea581a5e072964124aa2d3a41a38040
titus.renouf <titus.renouf@bluescale.com>
2022-12-05 15:37:43 +0100

    Button GA test 3

--- main.py
+++ main.py
@@ -2,16 +2,10 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
-@app.route("/", methods=['GET', 'POST'])
-def index():
-    if request.method == 'POST':
-        if request.form.get('action1') == 'VALUE1':
-            pass  # do something
-        elif request.form.get('action2') == 'VALUE2':
-            pass  # do something else
-        else:
-            pass  # unknown
-    elif request.method == 'GET':
-        return render_template('index.html', form='form')
-
+@app.route("/")
+@app.route("/home")
+def home():
     return render_template("index.html")
+
+if __name__=="__main__":
+    app.run(debug=True)
