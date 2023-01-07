d8b83d552b65f16f0b8d4b553245aced0178a86e
titus.renouf <titus.renouf@bluescale.com>
2023-01-07 18:47:31 +0100

    tp3

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+from pytrends.request import TrendReq
 from flask import Flask, render_template, request
 import requests
+from datetime import datetime
+
 app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
@@ -52,6 +55,64 @@ def myreqGA():
     app.logger.info(req)
     return req.text
 
+
+@app.route('/pytrends')
+def googletrendchart():
+    pytrends = TrendReq()
+    pytrends.build_payload(kw_list=["Lionel Messi", "Cristiano Ronaldo"], timeframe='today 90-d', geo='FR')
+    df = pytrends.interest_over_time()
+    data_messi = df['Lionel Messi'].tolist()
+    data_ronaldo = df['Cristiano Ronaldo'].tolist()
+    data_date = df.index.values.tolist()
+    timestamp_in_seconds = [element/1e9 for element in data_date]
+    date = [datetime.fromtimestamp(element)for element in timestamp_in_seconds]
+    days = [element.date() for element in date]
+    months = [element.isoformat() for element in days]
+    params = {
+        "type": 'line',
+        "data": {
+            "labels": months,
+            "datasets": [{
+                "label": 'Lionel Messi',
+                "data": data_messi,
+                "borderColor": '#3e95cd',
+                "fill": 'false',
+            },
+                {
+                "label": 'Cristiano Ronaldo',
+                "data": data_ronaldo,
+                "borderColor": '#ffce56',
+                "fill": 'false',
+            }
+            ]
+        },
+        "options": {
+            "title": {
+                # + str(topic_1) + " et " + str(topic_2)
+                "text": 'Comparaison entre'
+            },
+            "scales": {
+                "yAxes": [{
+                    "ticks": {
+                        "beginAtZero": 'true'
+                    }
+                }]
+            }
+        }
+    }
+
+    prefix_chartjs = """
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.min.js"></script>
+         <canvas id="myChart" width="1200px" height="700px"></canvas>""" + f"""
+        <script>
+        var ctx = document.getElementById('myChart');
+        var myChart = new Chart(ctx, {params});
+        </script>
+        """
+
+    return prefix_chartjs
+
+    
 
 if __name__=="__main__":
     app.run(debug=True)
